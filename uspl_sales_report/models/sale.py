727bca78eee8a19a98816e0079ebffbe3e5375e7
bablu <bablu>
2026-01-02 23:27:55 +0530

    Added changes

--- uspl_sales_report/models/sale.py
+++ uspl_sales_report/models/sale.py
@@ -15,25 +15,39 @@ class SaleOrderLine(models.Model):
         string="Unit Price",
         compute='_compute_price_unit_custom',
         digits='Product Price',
-        store=True, readonly=False, required=True, precompute=True)
+        store=True, readonly=False, required=True)
     
-    @api.depends('product_template_id', 'product_uom', 'product_uom_qty')
+    @api.depends('product_id', 'product_uom', 'product_uom_qty')
+    def _compute_price_unit(self):
+        pass
+
+    def _reset_price_unit(self):
+        pass
+
+    @api.depends('product_template_id', 'product_id',  'product_uom', 'product_uom_qty')
     def _compute_price_unit_custom(self):
         for rec in self:
+            if not rec.product_template_id:
+                continue
+            print('\n===rec', rec.product_template_id)
+            print('=rec', rec.product_id)
+            product_template_id = rec.product_id.product_tmpl_id
+            print('template_id', product_template_id)
             is_old = True
             pricelist_item_line = rec.order_id.pricelist_id.item_ids.filtered(lambda l: l.product_tmpl_id.id == rec.product_template_id.id)
+            company = rec.company_id.id
+            price_unit = rec.product_id.with_company(company).lst_price
+            discount = 0
             if pricelist_item_line:
                 line = pricelist_item_line.base_pricelist_id.item_ids.filtered(lambda l: l.product_tmpl_id.id == rec.product_template_id.id)
                 if line:
-                    rec.discount = pricelist_item_line.percent_price
-                    rec.price_unit = line.fixed_price
+                    discount = pricelist_item_line.percent_price
+                    price_unit = line.fixed_price
                 else:
-                    rec.price_unit = pricelist_item_line.fixed_price
-                    rec.discount = pricelist_item_line.percent_price
-            else:
-                company = rec.company_id.id
-                rec.price_unit = rec.product_id.with_company(company).lst_price
-                rec.discount = 0
+                    price_unit = pricelist_item_line.fixed_priceif
+                    discount = pricelist_item_line.percent_price
+            rec.discount = discount
+            rec.price_unit = price_unit
 
     @api.depends('product_id', 'product_template_id')
     def _compute_mrp(self):
